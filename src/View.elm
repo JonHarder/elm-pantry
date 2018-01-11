5b43804bf5388e29d36241955d49b3f60b2c3144
Jonathan Harder <jonharder6@gmail.com>
2018-01-11 00:21:43 -0600

    initial style-elements refactor

--- src/View.elm
+++ src/View.elm
@@ -1,36 +1,59 @@
 module View exposing (view)
 
 
-import Html exposing (Html, button, input, option, text, div, select)
-import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (..)
+import Html exposing (Html)
+
+import Element exposing (..)
+import Element.Attributes exposing (..)
+
+import Style exposing (..)
+import Style.Color as Color
+import Color exposing (..)
 
 
 import Model exposing (..)
 import Msgs exposing (..)
 
 
-resourceOption : Resource -> Html Msg
-resourceOption resource =
-    option [value <| toString resource] [text <| toString resource]
+type MyStyles
+    = Title
+
+
+stylesheet =
+    Style.styleSheet
+        [ style Title
+              [ Color.background blue
+              , Color.text white
+              ]
+        ]
+
+
+-- resourceOption : Resource -> Html Msg
+-- resourceOption resource =
+--     option [value <| toString resource] [text <| toString resource]
 
 
 view : Model -> Html Msg
 view model =
-    div [] [
-         div [] [
-              select [ onInput SetResource ] (List.map resourceOption [Recipe, Ingredient])
-             ],
-         div [] [
-              text <| toString model.resourceType ++ " id: " ++ toString model.resourceId
-             ],
-         div [] [
-              text <| toString model.resourceType ++ ": " ++ model.resourceName
-             ],
-         div [] [
-              input [ placeholder "id", onInput SetId ] []
-             , button [ onClick (GetResource model.resourceType) ]
-                  [ text <| "Get " ++ toString model.resourceType ] 
-             ]
-        ]
+    Element.layout stylesheet <|
+        row Title [spacing 10, padding 10, fill]
+            [ el Title [] (text "hello style")
+            , el Title [] (text "foo bar")
+            ]
+    -- div [] [
+    --      div [] [
+    --           select [ onInput SetResource ] (List.map resourceOption [Recipe, Ingredient])
+    --          ],
+    --      div [] [
+    --           text <| toString model.resourceType ++ " id: " ++ toString model.resourceId
+    --          ],
+    --      div [] [
+    --           text <| toString model.resourceType ++ ": " ++ model.resourceName
+    --          ],
+    --      div [] [
+    --           input [ placeholder "id", onInput SetId ] []
+    --          , button [ onClick (GetResource model.resourceType) ]
+    --               [ text <| "Get " ++ toString model.resourceType ] 
+    --          ]
+    --     ]
 
